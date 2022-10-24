27552f644dce4f59db385bb55d7f1a8ad78c2fef
Theminda Srimal <67902627+ThemindaSrimal@users.noreply.github.com>
2022-10-24 10:16:48 +0530

    Create SelectionSort.py

---
+++ SelectionSort.py
@@ -0,0 +1,21 @@
+#function for selection sort
+
+def selectionSort( itemsList ):
+    n = len( itemsList )
+    for i in range( n - 1 ): 
+        minValueIndex = i
+
+        for j in range( i + 1, n ):
+            if itemsList[j] < itemsList[minValueIndex] :
+                minValueIndex = j
+
+        if minValueIndex != i :
+            temp = itemsList[i]
+            itemsList[i] = itemsList[minValueIndex]
+            itemsList[minValueIndex] = temp
+
+    return itemsList
+  
+# example
+# el = [21,6,9,33,3]
+# print(selectionSort(el))
